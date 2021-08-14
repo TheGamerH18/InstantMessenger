0e3cbc563edb79d4cb303774558bccae46a08c6a
svenibo12 <57830909+svenibo12@users.noreply.github.com>
2021-08-14 20:59:31 +0200

    Added Login

--- InstantMessenger/Controllers/LoginController.cs
+++ InstantMessenger/Controllers/LoginController.cs
@@ -9,21 +9,16 @@ using System.Threading.Tasks;
 
 namespace InstantMessenger.Controllers
 {
-    public class HomeController : Controller
+    public class LoginController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<LoginController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public LoginController(ILogger<LoginController> logger)
         {
             _logger = logger;
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
