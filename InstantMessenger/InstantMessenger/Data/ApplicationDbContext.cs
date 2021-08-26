9bdf94acc0bea371f0e5ad3478e5f6764495167f
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-08-26 10:20:17 +0200

    Add new Chat Page with Database

--- InstantMessenger/InstantMessenger/Data/ApplicationDbContext.cs
+++ InstantMessenger/InstantMessenger/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using InstantMessenger.Models;
 
 namespace InstantMessenger.Data
 {
@@ -12,5 +13,6 @@ namespace InstantMessenger.Data
             : base(options)
         {
         }
+        public DbSet<InstantMessenger.Models.Chat> Chat { get; set; }
     }
 }
---
+++ InstantMessenger/InstantMessenger/Models/Chat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstantMessenger.Models
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime date { get; set; }
+        public string sender { get; set; }
+        public string reciever { get; set; }
+    }
+}
