645c122b7ea6294b1edaeda230afc92268558536
svenibo12 <57830909+svenibo12@users.noreply.github.com>
2021-09-05 19:33:22 +0200

    Added the CSS and JS
    Take a look at the chat.js file to see how to send the Data, must be formatted like this

---
+++ InstantMessenger/InstantMessenger/wwwroot/js/chatJS/chat.js
@@ -0,0 +1,14 @@
+let Chats = Array(
+    {UserName: "Testuser", UserID: 1337, Messages: [[from = false, message = "laksdkasjd"],[from = true, message = 1],[from = false, message = 1],[from = false, message = 1],[from = true, message = 1]]},
+    {UserName: "Sven", UserID: 1338,     Messages: [[from = false, message = "laksdkasjd"],[from = true, message = 1],[from = false, message = 1],[from = true, message = 1],[from = true, message = 1]]},
+    {UserName: "Sven2", UserID: 138,     Messages: [[from = false, message = "laksdkasjd"],[from = true, message = 1],[from = false, message = 1],[from = true, message = 1],[from = true, message = 1]]},
+    {UserName: "Sven3", UserID: 1347,    Messages: [[from = false, message = "laksdkasjd"],[from = true, message = 1],[from = false, message = 1],[from = true, message = 1],[from = true, message = 1]]},
+    {UserName: "Sven4", UserID: 1335,    Messages: [[from = false, message = "laksdkasjd"],[from = true, message = 1],[from = false, message = 1],[from = true, message = 1],[from = true, message = 1]]},
+    {UserName: "Sven5", UserID: 1386,    Messages: [[from = false, message = "laksdkasjd"],[from = true, message = 1],[from = false, message = 1],[from = true, message = 1],[from = true, message = 1]]},
+    {UserName: "Sven6", UserID: 1737,    Messages: [[from = false, message = "laksdkasjd"],[from = true, message = 1],[from = false, message = 1],[from = true, message = 1],[from = true, message = 1]]},
+    {UserName: "Sven7", UserID: 1388,    Messages: [[from = false, message = "laksdkasjd"],[from = true, message = 1],[from = false, message = 1],[from = true, message = 1],[from = true, message = 1]]},
+    {UserName: "Sven8", UserID: 13888,   Messages: [[from = false, message = "laksdkasjd"],[from = true, message = 1],[from = false, message = 1],[from = true, message = 1],[from = true, message = 1]]},
+    {UserName: "Sven9", UserID: 13567,   Messages: [[from = false, message = "laksdkasjd"],[from = true, message = 1],[from = false, message = 1],[from = true, message = 1],[from = true, message = 1]]},
+    {UserName: "Sven10", UserID: 13356,  Messages: [[from = false, message = "laksdkasjd"],[from = true, message = 1],[from = false, message = 1],[from = true, message = 1],[from = true, message = 1]]},
+    {UserName: "Sven11", UserID: 1381,   Messages: [[from = false, message = "laksdkasjd"],[from = true, message = 1],[from = false, message = 1],[from = true, message = 1],[from = true, message = 1]]}
+);
---
+++ InstantMessenger/InstantMessenger/wwwroot/js/chatJS/chatFetcher.js
@@ -0,0 +1,165 @@
+let ExistingChats = new Array();
+
+// Clear the Chat Window, to display the new Chat
+let clearChatMessages = () => {
+    var el = document.querySelector('.openChat');
+
+    while ( el.firstChild ) el.removeChild( el.firstChild );
+};
+
+// The Main Renderer
+const mainRender = () => {
+    for(i = 0; i < ExistingChats.length; i++) {
+        if(ExistingChats[i].isActive) {
+            ExistingChats[i].render();
+            updater();
+        }
+    }
+    setTimeout(mainRender, 1000/30)
+}
+
+
+// Update if the Document gets an Update
+document.onreadystatechange = () => {
+    updater();
+}
+
+const updater = () => {
+    
+    // Loop through every Chat and Check if it needs to be created or not
+    for(i = 0; i < Chats.length; i++) {
+        if(exists(Chats[i]))
+            ExistingChats[i].update(Chats[i].Messages);
+        else
+            ExistingChats[ExistingChats.length] = new Chat(Chats[i]);
+    }
+}
+
+// check if the chat is already existing
+let exists = (newChat) => {
+    for(chat in ExistingChats) {
+        // check also if the Type is the Same 
+        if(newChat.UserID === ExistingChats[chat].id)
+            return !0;
+    }
+    return !1;
+}
+
+// Set the Chat with the ID to Active, to get it visible
+let times = 0;
+
+// To set the clicked chat to active
+// Needed in the Renderer to know what needs to be Rendered
+let setActiveID = (thisID) => {
+
+    for(i = 0; i < ExistingChats.length; ++i) {
+        if(ExistingChats[i].id == thisID) {
+            ExistingChats[i].setActive(!0);
+        } else {
+            ExistingChats[i].setActive(!1);
+        }
+    }
+}
+
+// Keep the names in Json the same
+// The Chat as a Object, for more Accessibility
+class Chat{
+
+    // The Constructor, the Syntax of the Json must be the Same
+    constructor({
+        UserName: name,
+        UserID: id,
+        Messages: messages = new Array,
+    } = {}) {
+
+        this.name = name;
+        this.id = id;
+        this.messages = messages;
+        this.isActive = !1;
+        
+        /*
+          Create the DOM Elements
+        */
+        this.div = document.createElement("div");
+        this.div.setAttribute("class", "userChat");
+
+        this.userImg = document.createElement("img");
+        this.userImg.setAttribute("class", "userImg");
+        this.userImg.setAttribute("src", "~/img/fallbackImg.jpg");
+
+        this.content = document.createElement("div");
+        this.content.setAttribute("class", "content");
+
+        this.userName = document.createElement("p");
+        this.userName.setAttribute("class", "userName");
+        this.userName.innerHTML = this.name;
+
+        this.latestMes = document.createElement("p");
+        this.latestMes.setAttribute("class", "newestMessage");
+        this.latestMes.innerHTML = this.messages[this.messages.length-1][1];
+
+        // Append the Elements
+        document.querySelector(".chats").appendChild(this.div);
+
+        this.div.appendChild(this.userImg);
+        this.div.appendChild(this.content);
+        
+        this.content.appendChild(this.userName);
+        this.content.appendChild(this.latestMes);
+
+        // Add a Click Listener
+        this.div.setAttribute("onClick", "setActiveID("+this.id+")");
+    }
+
+    // Update the Messages
+    update(messages) {
+        this.messages = messages;
+    }
+    
+    setActive(bool) {
+        this.isActive = bool;
+        if(this.isActive) {
+            this.div.style.background = "red";
+        } else {
+            this.div.style.background = "";
+        }
+    }
+
+    render() {
+        clearChatMessages();
+        this.showChat();
+    }
+
+    showChat() {
+        for(let mes in this.messages){
+            if(this.messages[mes][0])
+                this.createFrom(this.messages[mes][1]);
+            else
+                this.createTo(this.messages[mes][1]);
+        }
+    }
+
+    createFrom (mes) {
+        let div = this.createMes(mes);
+        div.setAttribute("class", "message from");
+    }
+
+    createTo (mes) {
+        let div = this.createMes(mes);
+        div.setAttribute("class", "message you");
+    }
+
+    createMes (mes) {
+        let div = document.createElement("div");
+        let content = document.createElement("p");
+        content.setAttribute("class", "content");
+        content.innerHTML = mes;
+        let parent = document.querySelector(".openChat");
+        parent.appendChild(div);
+        div.appendChild(content);
+
+        return div;
+    }
+}
+
+mainRender();
