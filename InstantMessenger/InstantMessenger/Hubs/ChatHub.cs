bbe022bd9f11b2b51d31cb41220a2f785e08c7bd
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-09-08 08:55:57 +0200

    Adding functions to hub

--- InstantMessenger/InstantMessenger/Hubs/ChatHub.cs
+++ InstantMessenger/InstantMessenger/Hubs/ChatHub.cs
@@ -29,16 +29,57 @@ namespace InstantMessenger.Hubs
             var messages = _context.Chat.Where(p =>
                 p.reciever == user.Result
                 || p.sender == user.Result);
+            List<Dictionary<string, dynamic>> chats = new();
             Clients.Group(Context.User.Identity.Name).SendAsync("messages", chats);
             return base.OnConnectedAsync();
         }
 
+        private List<Dictionary<string, dynamic>> AddmessagetoArray(Models.Chat message, List<Dictionary<string, dynamic>> chats)
+        {
+            var contextuser = _userManager.FindByNameAsync(Context.User.Identity.Name).Result;
+            var isfromuser = (message.reciever.UserName != contextuser.UserName);
+            // Create Dictionary, which holds if the user is the sender and the message itself
+            Dictionary<string, dynamic> newmessage = new();
+            newmessage.Add("from", isfromuser);
+            newmessage.Add("message", message.Text);
+
+            // Add message to existing Chat, if existing and returning
+            for (int i = 0; i < chats.Count; i++)
+            {
+                if(chats[i]["UserName"] == message.reciever.UserName &&
+                    chats[i]["UserID"] == message.reciever.Id)
+                {
+                    chats[i]["Message"].append(newmessage);
+                    return chats;
+                }
+            }
+
+            // Create new Chat
+            var newchat = NewChat(isfromuser, message);
+            newchat["Message"].append(newmessage);
+            _ = chats.Append(newchat);
+            return chats;
+        }
+
+        private static Dictionary<string, dynamic> NewChat(bool isfromuser, Models.Chat message)
+        {
+            var User = isfromuser ? message.reciever : message.sender;
+            Dictionary<string, dynamic> newchat = new();
+            newchat.Add("UserName", User.UserName);
+            newchat.Add("UserID", User.Id);
+            newchat.Add("Messages", new List<Dictionary<string, dynamic>>());
+
+            return newchat;
+        }
+
         public async Task SendtoUser(string sender, string reciever, string message)
         {
-            Models.Chat messagetodb = new Models.Chat();
-            messagetodb.reciever = _userManager.FindByNameAsync(reciever).Result;
-            messagetodb.sender = _userManager.FindByNameAsync(Context.User.Identity.Name).Result;
-            messagetodb.Text = message;
+            Models.Chat messagetodb = new()
+            {
+                reciever = _userManager.FindByNameAsync(reciever).Result,
+                sender = _userManager.FindByNameAsync(Context.User.Identity.Name).Result,
+                Text = message
+            };
 
             await _context.Chat.AddAsync(messagetodb);
             await Clients.Group(reciever).SendAsync("Recieve Message", sender, message);
